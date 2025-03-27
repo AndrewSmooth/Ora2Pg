b751b7a4e304c46768205f52d1fda4d8e08cee62
GiG789 <TECNO@WIN-1HGHN4C8HMQ.com>
2025-03-27 18:46:48 +0300

    IP and NC

---
+++ IP.py
@@ -0,0 +1,4 @@
+def instr_to_position(name_func):
+    return name_func.replace("INSTR", "POSITION")
+
+
---
+++ NC.py
@@ -0,0 +1,3 @@
+def to_number_to_cast(name_func):
+    return name_func.replace("TO_NUMBER", "CAST")
+
